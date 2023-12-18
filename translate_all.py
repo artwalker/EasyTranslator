c03557321e8f4963dbe3e1d157ffe40752f80141
xinxing.wang <xinxing.wang@megatronix.co>
2023-12-18 13:57:51 +0800

    Create a script for batch translating documents. The command to run is .

---
+++ translate_all.py
@@ -0,0 +1,18 @@
+import os
+import glob
+from termcolor import colored
+
+# Get a list of all text files in the book directory
+file_list = glob.glob('./book/*.txt')
+
+# Sort the files by their numerical prefix
+file_list.sort(key=lambda x: int(x.split('_')[0].split('/')[-1]))
+
+# Use your translation function on each file
+for file in file_list:
+    # Print the name of the file you're translating
+    print(colored(f'Now translating {file}', 'magenta'))
+    os.system(f'python easy_translator.py {file}')
+
+# Print a message when you're done
+print(colored('All done!', 'magenta'))
