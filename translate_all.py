50858ef95f9ca03c4ca0def139a5aac4a49d2a2b
Ethan Wang <wxinxings@icloud.com>
2023-12-19 14:29:33 +0000

    Update file_list sorting for Linux and Windows versions

--- translate_all.py
+++ translate_all.py
@@ -3,10 +3,16 @@ import glob
 from termcolor import colored
 
 # Get a list of all text files in the book directory
+# Linux version
 file_list = glob.glob('./book/*.txt')
+# Windows version
+# file_list = glob.glob('.\\book\\*.txt')
 
 # Sort the files by their numerical prefix
+# Linux version
 file_list.sort(key=lambda x: int(x.split('_')[0].split('/')[-1]))
+# Windows version
+# file_list.sort(key=lambda x: int(x.split('_')[0].split('\\')[-1]))
 
 # Use your translation function on each file
 for file in file_list:
